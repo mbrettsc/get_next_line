ed6decd0b49872d2df8dc644e781054a04f6b7ac
Martin Brettschneider <burakmartin007@outlook.com>
2022-10-23 18:48:01 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrettsc <mbrettsc@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 16:12:31 by mbrettsc          #+#    #+#             */
+/*   Updated: 2022/10/23 15:40:28 by mbrettsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+char	*get_next_line(int fd);
+
+#endif
